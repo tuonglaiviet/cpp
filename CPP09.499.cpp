c4741ad361724bc70b2df4921801daedda845fd2
tuonglaiviet <lhtrung@gmail.com>
2023-02-26 16:28:04 +0700

    Update CPP09.499.cpp

--- CPP09.499.cpp
+++ CPP09.499.cpp
@@ -3,6 +3,9 @@
   * Đệ quy tuyến tính
   Bài 499: Tính S(n) = 1 + 2 + 3 + … + n
   ÂN Group - www.angroup.one
+  
+  Ghi chú: Đệ quy tuyến tính (Linear Recursion) - Đệ quy tuyến tính là hàm đệ quy chỉ gọi chính nó một lần trong thân hàm. Hiểu đơn giản là trong một hàm, nếu có duy nhất một câu lệnh gọi chính hàm đó thì được gọi là hàm đệ quy tuyến tính.
+           Ví dụ hàm tính giai thừa chính là một hàm đệ quy tuyến tính, vì nó chỉ gọi lại chính nó một lần duy nhất.
 */
 
 #include<stdio.h>
