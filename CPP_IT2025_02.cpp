f9c6cd37f03afc025a4a60f4ef0a52a358d5bf0c
tuonglaiviet <lhtrung@gmail.com>
2023-08-11 23:14:22 +0700

    Create CPP_IT2025_02.cpp

---
+++ CPP_IT2025_02.cpp
@@ -0,0 +1,29 @@
+// You are familiar with producing output using C++/C. In this task, you'll be required to write three messages in three separate lines. Write I love The  N. But wait a minute!! You need to write each word of it in a separate line.
+// Input: No Input
+// Output:
+//    I
+//    love
+//    The ÂN
+// Explanation: First word of the statement is in the first line, next word is in the next line, and last is in the last line.
+// Your Task: In the function printInNewLine(), output each word of I love The ÂN in a separate line.
+// ---------------------------------------------------------------------------------------------------
+
+//{ Driver Code Starts
+//Initial Template for C++
+#include<iostream>
+using namespace std;
+void printNewLine();
+// } Driver Code Ends
+
+//User function Template for C++
+// Function to print each word in new line
+void printNewLine(){
+    // Your code here
+}
+
+//{ Driver Code Starts.
+int main(){
+    //function call
+    printNewLine();
+}
+// } Driver Code Ends
