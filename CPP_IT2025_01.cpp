ac53246076c2ffd05156ae324c3d5971b832290a
tuonglaiviet <lhtrung@gmail.com>
2023-08-11 23:11:22 +0700

    Update CPP_IT2025_01.cpp

--- CPP_IT2025_01.cpp
+++ CPP_IT2025_01.cpp
@@ -1,3 +1,8 @@
+// Let's begin your journey towards coding with the very first question of the coding world. Your task is to write code which prints Hello World.
+// Input: No Input
+// Output: Hello World
+// Your Task: In the function helloWorld(), output a line "Hello World" (without quotes).
+// ---------------------------------------------------------------------------------------------------
 //{ Driver Code Starts
 #include<iostream>
 using namespace std;
